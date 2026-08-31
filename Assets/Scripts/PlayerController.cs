778f1933be577c7269d08eafcb32ddc4d6004cf8
Victor Souza <victorsouza.software@gmail.com>
2026-08-30 23:22:13 -0300

    feat: estrutura inicial do projeto, mecanica do player está pronta

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Variaveis Comuns")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float lookSpeed = 120f;
+    [SerializeField] private float minCameraAngle = -60f;
+    [SerializeField] private float maxCameraAngle = 60f;
+
+    [Header("Variaveis de Controle")]
+    [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private InputActionReference jumpAction;
+    [SerializeField] private InputActionReference lookAction;
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private Transform cameraTransform;
+
+    private float ySpeed;
+    private float cameraVerticalRotation;
+
+    private void Update()
+    {
+        Vector2 lookInput = lookAction.action.ReadValue<Vector2>();
+        Vector2 moveInput = moveAction.action.ReadValue<Vector2>();
+
+        float horizontalLook = lookInput.x * lookSpeed * Time.deltaTime;
+        float verticalLook = lookInput.y * lookSpeed * Time.deltaTime;
+
+        transform.Rotate(0f, horizontalLook, 0f);
+
+        cameraVerticalRotation -= verticalLook;
+        cameraVerticalRotation = Mathf.Clamp(
+            cameraVerticalRotation,
+            minCameraAngle,
+            maxCameraAngle
+        );
+
+        cameraTransform.localRotation =
+            Quaternion.Euler(cameraVerticalRotation, 0f, 0f);
+
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveAmount =
+            right * moveInput.x +
+            forward * moveInput.y;
+
+        if (moveAmount.magnitude > 1f)
+            moveAmount.Normalize();
+
+        moveAmount *= moveSpeed;
+
+        if (characterController.isGrounded)
+        {
+            if (ySpeed < 0f)
+                ySpeed = -2f;
+
+            if (jumpAction.action.WasPressedThisFrame())
+                ySpeed = jumpForce;
+        }
+        else
+        {
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+        }
+
+        moveAmount.y = ySpeed;
+
+        characterController.Move(moveAmount * Time.deltaTime);
+    }
+}
